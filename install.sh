e1bbbe288a31a5d079102b9d81e0fb28e6a4bd07
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-22 03:37:49 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+apt-get install -y atomicparsley
+apt-get install -y ffmpeg
+apt autoremove
+chmod +x "/content/ydl/downvideo.sh"
+chmod +x "/content/ydl/downmp3.sh"
