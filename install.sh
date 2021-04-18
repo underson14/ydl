55bfe7e35f28305d8b486796ec9d7cd75a1e41ec
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-04-18 15:39:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
-apt-get install -y atomicparsley
-apt-get install -y ffmpeg
+apt install -y atomicparsley
+apt install -y ffmpeg
+apt install axel -y
 apt autoremove
 chmod +x "/content/ydl/downvideo.sh"
 chmod +x "/content/ydl/downmp3.sh"
