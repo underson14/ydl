e03a9f7cdd845073b61212acb6436c017b9ad0ac
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-06-06 23:31:17 -0300

    Update downvideo.sh

--- downvideo.sh
+++ downvideo.sh
@@ -1,4 +1,5 @@
-youtube-dl --autonumber-start 1 -ci --hls-prefer-native \
---add-metadata --write-auto-sub --embed-thumbnail -f \
+time youtube-dl --autonumber-start 1 -ci --hls-prefer-native \
+--add-metadata --embed-thumbnail -f \
 'bestvideo[height<=?1080][fps<=?30][vcodec!=?vp9]+bestaudio[ext=m4a]/best' \
 -o "${1}/%(autonumber)s.%(title)s.%(ext)s" $2
+
