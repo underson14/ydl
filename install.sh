02e6d3aef32c4e43b4e9b862ab00835bdddb4fd5
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-07-25 15:28:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ sudo chmod a+rx /usr/local/bin/youtube-dl
 apt install -y atomicparsley
 git clone https://github.com/underson14/colab-ffmpeg-cuda.git
 cp -r ./colab-ffmpeg-cuda/bin/. /usr/bin/
-apt install axel -y
+apt install aria2 -y
 apt autoremove
 chmod +x "/content/ydl/downvideo.sh"
 chmod +x "/content/ydl/downmp3.sh"
-pip3 install tidal-dl --upgrade
